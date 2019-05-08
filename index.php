4f0e1706db7aceed9b54a66690e60d8bf99af868
Veysel Baskan <veysel.baskani@optdcom.net>
2019-05-08 14:40:02 +0300

    index.php title olusturuldu.

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Git System</title>
   </head>
   <body>
-    
+      <?php
+
+
+      ?>
   </body>
 </html>
